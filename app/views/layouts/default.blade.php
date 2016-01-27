c1b20c9d2e44d714a47b8b5a3e94e95c9bc4a25d
RedbackThomson <redbackthomson@gmail.com>
2016-01-26 20:38:37 -0800

    Added all meta tags

--- app/views/layouts/default.blade.php
+++ app/views/layouts/default.blade.php
@@ -13,6 +13,8 @@
 	<meta property="og:description" content="The hand-crafted, scrupulously designed and beatifully implemented portfolio of Redback. Young developer with experience in software, web and mobile development.">
 	<meta property="og:image" content="https://rdbk.tv/img/RBLogo.png">
 	<meta property="og:image:url" content="https://rdbk.tv/img/RBLogo.png">
+	<meta property="og:image:secure_url" content="https://rdbk.tv/img/RBLogo.png">
+	<meta property="og:image:type" content="image/png">
 	<meta property="og:image:width" content="1203">
 	<meta property="og:image:height" content="784">
 </head>
